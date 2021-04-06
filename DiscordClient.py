e25df7a0de3531f941358770e5e4917084ebe1dd
Alex Drewno <adrewno14@gmail.com>
2021-04-06 01:26:39 -0500

    finished chatty-bot

--- DiscordClient.py
+++ DiscordClient.py
@@ -1,6 +1,9 @@
 import discord
+from MongoDB import MongoDB
 
 class DiscordClient(discord.Client):
+    db = None
+
     async def on_ready(self):
         """
             Implementing discord.Client on_ready() that is called when the bot is ready
@@ -8,13 +11,17 @@ class DiscordClient(discord.Client):
             We do any additional post-initialization set-up here
         """
         print('Logged on as {0}!'.format(self.user))
-    
+        self.db = MongoDB()
+
+    async def on_reaction_add(self, reaction, user):
+        self.db.addReactToMessageInDb(reaction)
+        
     async def on_message(self, message):
         """
-            Implementing discord.Client on_message() that is called when a user messages
-            in a server (discord.Guild)
+        Implementing discord.Client on_message() that is called when a user messages
+        in a server (discord.Guild)
 
-            This is where all of the commands are called for the DiscordClient
+        This is where all of the commands are called for the DiscordClient
         """
 
         if message.author == self.user:
@@ -22,6 +29,8 @@ class DiscordClient(discord.Client):
 
         if len(message.content) < 1:
             return
+        
+        self.db.addMessageToDb(message)
 
- 
+
 
---
+++ MongoDB.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MongoDB: 
+
+    def __init__ (self):
+        self.client = MongoClient(os.getenv('MONGO_URI'))
+        self.db = self.client.ChatLogs
+    
+    def addMessageToDb(self, message):
+        messageDict = {
+            "_id": message.id,
+            "date": datetime.utcnow().isoformat(),
+            "authorName": message.author.name,
+            "authorDiscriminator": message.author.discriminator,
+            "guild": message.guild.name,
+            "channel": message.channel.name,
+            "reactions": [],
+            "message": message.content,
+        }
+
+        self.db.logs.insert_one(messageDict)
+
+    def addReactToMessageInDb(self, reaction):
+        messageDict = self.db.logs.find_one({"_id": reaction.message.id})
+
+        if reaction.custom_emoji:
+            messageDict["reactions"].append(reaction.emoji.name)
+        else:
+            messageDict["reactions"].append(reaction.emoji)
+
+        myQuery = { "_id": reaction.message.id }
+        updatedReactions = { "$set": { "reactions": messageDict["reactions"] } }
+
+        self.db.logs.update_one(myQuery, updatedReactions)
+    
